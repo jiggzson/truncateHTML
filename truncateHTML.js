a325d21ff75ca5db41f76e0ed9af1fda996efd1d
Martin Donk <jiggzson@gmail.com>
2014-04-14 18:50:16 -0400

    Update truncateHTML.js

--- truncateHTML.js
+++ truncateHTML.js
@@ -6,24 +6,24 @@
 */
 
 (function($){
-    //This method works by first removing all the tags, then chopping the text and then
-    //reinserting the tags excluding those which may alter the formatting after the chop.
+    //This method works by first removing all the tags, then truncating the text and then
+    //reinserting without breaking the html
     $.fn.truncateHTML = function() {
         var userOptions = typeof arguments[0] === 'object' ? [].shift.apply(arguments) : {};
 
         if(arguments[0]) userOptions[arguments[0]] = arguments[1];
-        //grab the html and store it
         //prepare the user provided options
         var $this = (this),
             html = $this.html().trim(),
             //the container we'll use to store the tags
             tags = [],
-            //the tags which are to be excluded when tags are reinserted
+            //the tags which are to be explicitly excluded when tags are reinserted
+            //after truncating
             conditionals = ['img', 'button', 'br', 'hr', 'input', 'textarea', 'video'],
             defaults =  {
                 ellipsis: '...',
                 chararcters: html.length,
-                forceWord: false
+                wordBoundary: false
             },
             options = $.extend(true, defaults, userOptions),
 
@@ -41,7 +41,7 @@
 
         //if the cut should not cut words then we have to check if the cut is followed by a letter. 
         //if so then we have to remove the last letter altogether.
-        if(options.forceWord && text.charAt(options.characters+1) !== SPACE) {
+        if(options.wordBoundary && text.charAt(options.characters+1) !== SPACE) {
             var textArray = text.split(SPACE).splice(0, text.length-1);
             textArray.pop();
             text = textArray.join(SPACE);
@@ -49,27 +49,31 @@
 
         //add the ellipsis
         text += options.ellipsis;
-        
+  
         //put back the tags
-        var l = tags.length;
+        var l = tags.length,
+            deadTags = [];
         for(i=0; i<l; i++) {
             var position = tags[i][1],
                 tag = tags[i][0];
-
-            //It should be safe to just reinsert all the tags with the exception of a few due 
-            //to the fact that they'll have zero width and therefore won't break formatting 
-            //e.g. <a href="somelink"></a> shouldn't break formatting.
-            //Some tags however are not worthy and must be slain. TODO: remove "dead" tags
             if(position > options.characters) { 
-                var tagname = /<\/*(\w+)/gi.exec(tag.split(SPACE).shift())[1].toLowerCase();
-                if($.inArray(tagname, conditionals) > -1) tag = '';
+                var matches = /<(\/*)(\w+)/gi.exec(tag.split(SPACE).shift()),
+                    tagname = matches[2].toLowerCase();
+                if($.inArray(tagname, conditionals) > -1) { tag = ''; }	
+                //If any closing tag is found it's removed
+                if(matches[1] ==='/' && deadTags[deadTags.length-1] === tagname) { deadTags.pop();}
+                else if(tag) { deadTags.push(tagname); }
             }
-            text = text.substr(0, position)+tag+text.substr(position);
+            else {
+                text = text.substr(0, position)+tag+text.substr(position);
+            }
         }
-        
+        //the remaining tags should only be closing tags
+        text += deadTags.map(function(tag){return '</'+tag+'>';}).join('');
+		
         //replace the html within the container
-        $this.html(text);
-        
+        $this.html(text); 
+
         return $this;
     };    
 })(jQuery);
